d15e318f71a8f8f2f9207af375b6df36e8b9daf8
Stephen Fox <stevethecoder34@gmail.com>
2017-03-08 23:09:08 -0700

    Parallelize simply

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,10 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then(response => {
+        response.results.map(url => 
+          getJSON(url).then(createPlanetThumb))
+      })
   });
 })(document);
